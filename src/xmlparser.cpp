400961cd8354ff03e417025fc203bab1cb60e2c9
stephamd <stephamd@mail.uc.edu>
2017-05-16 22:32:37 -0400

    adding xmlparser.cpp to repo.  Compiles, still working on xml parser class

---
+++ src/xmlparser.cpp
@@ -0,0 +1,92 @@
+/* xmlparser.cpp
+ *
+ * Copyright © TechSolve Inc. 2013-2016,
+ * All rights reserved.
+ * 
+ * Use without explicit consent in writing is forbidden.
+ *
+ */
+
+
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <assert.h>
+#include "xmlparser.hpp"
+
+
+using namespace std;
+
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+
+//#if defined(LIBXML_XPATH_ENABLED) && defined(LIBXML_SAX1_ENABLED)
+
+//extern "C" void XMLCDECL
+
+bool debug1 = true;
+
+XmlParser::XmlParser()
+{
+}
+
+XmlParser::XmlParser(const char* _filename)
+{
+    filename.assign(_filename);
+}
+
+XmlParser::~XmlParser()
+{
+}
+
+int XmlParser::openDoc()
+{
+    assert(filename.c_str());
+    if (debug1) cout << "Open Document" << endl;
+    doc = xmlParseFile(filename.c_str());
+    if (debug1) cout << "Document Opened" << endl;
+
+    //close and return if file is not formatted correctly
+    if (doc == NULL){
+	printf("Document not parsed successfully\n");
+        xmlFreeDoc(doc);
+	return 0;
+    }
+
+    cur = xmlDocGetRootElement(doc);    
+
+    if (cur == NULL){
+	printf("Document Empty");
+	xmlFreeDoc(doc);
+	return 0;
+    }
+
+    if (xmlStrcmp(cur->name, (const xmlChar *) "response")){
+	printf("XML Doc is not of correct type");
+	xmlFreeDoc(doc);
+	return 0;
+    }
+    
+    printf("Document Parsed Successfully\n");
+    return 1; 
+
+}
+
+
+vector<vector<string> > XmlParser::getInfo(vector<string> vec)
+{
+    vector<vector<string> > info;
+        
+    return info;
+}
+
+int XmlParser::cleanUp()
+{
+    /* Cleanup */ 
+    xmlFreeDoc(doc); 
+    return(0);
+}
