72987199f02986180b0de0512f08624f27b34221
Matt Stephan <stephan@techsolve.org>
2017-06-12 10:24:39 -0400

    Initial project

--- src/weatherReport.cpp
+++ src/weatherReport.cpp
@@ -5,54 +5,55 @@ Weather reporting using curl and weather underground api
 
 author: Matt Stephan
 
-*/
+ */
 
 
 
 //includes
 
 #include <stdio.h>
+#include <stddef.h>
 #include <string>
 #include <curl/curl.h>
-#include "includes/xmlparser.hpp"
+#include "xmlparser.hpp"
 
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream){
-	size_t written = fwrite(ptr, size, nmemb, stream);
-	return written;
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
 }
 
+int main(int argc, char* argv[]) {
 
-int main(int argc, char* argv[]){
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    char *url = "http://api.wunderground.com/api/f20b91a98c48d70f/conditions/q/OH/Cincinnati.xml";
+    char outfilename[FILENAME_MAX] = "./res/weather_report.xml";
 
-	CURL *curl;
-	FILE *fp;
-	CURLcode res;
-	char *url = "http://api.wunderground.com/api/f20b91a98c48d70f/conditions/q/OH/Cincinnati.xml";
-	char outfilename[FILENAME_MAX] = "./res/weather_report.xml";
+    //initialize objects
+    XmlParser parser = XmlParser("./res/weather_report.xml");
+    curl = curl_easy_init();
 
-	//initialize objects
-	XmlParser parser = XmlParser("./res/weather_report.xml");
-	curl = curl_easy_init();
+    if (curl) {
 
-	if(curl){
-		
-		printf("curl intialized properly\n");
+        printf("curl intialized properly\n");
 
 
-		fp = fopen(outfilename, "wb");
-		curl_easy_setopt(curl, CURLOPT_URL, url);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-		res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
-		fclose(fp);
-	}
-	
-	parser.openDoc();
-	
-	parser.cleanUp();
+        fp = fopen(outfilename, "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        fclose(fp);
+    }
 
-	return 0;	
+    parser.openDoc();
+
+    parser.cleanUp();
+
+    return 0;
 }
 
 
